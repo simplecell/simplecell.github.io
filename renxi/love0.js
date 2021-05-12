23358a719ab2ea1727068a6278335d246f227eec
simplecell <83950447+simplecell@users.noreply.github.com>
2021-05-12 14:24:02 +0800

    Update love0.js

--- renxi/love0.js
+++ renxi/love0.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-	    ctx.fillText(" [亲爱的---]", 23, 10);
+	    ctx.fillText(" [亲爱的老婆]", 23, 10);
             ctx.restore();
         },
         clear: function() {
